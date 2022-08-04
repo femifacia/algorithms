b69a2ebf6da881ebb29e63894af8670563c810c2
femifacia <femi.facia@epitech.eu>
2022-08-05 00:48:12 +0100

    [ADD] New algorithms

---
+++ python/invert_binary_tree/main_optimized.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Definition for a binary tree node.
+from operator import invert
+
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if (root == None):
+            return (None)
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return (root)
