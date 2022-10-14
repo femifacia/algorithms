894548b60b5e250e194b1ef6371b1ea21bf72953
femifacia <femi.facia@epitech.eu>
2022-10-14 03:03:08 +0100

    [ADD] Delete node in a linked list and add it with a c++ cool way lol

---
+++ python/algorithms/delete_node_in_a_linked_list/main.cpp
@@ -0,0 +1,6 @@
+    //static void deleteNode(ListNode* node) {
+    //    ListNode* next = node->next;
+    //    *node = *next;
+        // Don't be that person that leaks memory.
+    //    delete next;
+    //}
