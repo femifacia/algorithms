bfe1592fe78648d3e238d17827861dfabc20d3fb
femifacia <femi.facia@epitech.eu>
2022-10-30 04:02:34 +0100

    [UPD] Implement Trie by Adding comments on the efficient Trie

--- python/algorithms/implement_trie/trie_efficient.py
+++ python/algorithms/implement_trie/trie_efficient.py
@@ -5,8 +5,11 @@ from string import ascii_letters
 
 class TrieNode:
     def __init__(self) -> None:
+        #each node has to know if it is a word or not
         self.isWord = False
+        #each node has a dictionary which at a c char associate a TrieNode
         self.children = {}
+        #children could have been init like this self.children = {c : None for c in ascii_letters}
 
 
 class Trie:
@@ -15,19 +18,26 @@ class Trie:
         self.root = TrieNode()
 
     def insert(self, word: str) -> None:
+        #we start at the root
         current = self.root
         for i in word:
             if not i in current.children:
+                #if a letter of the word we want to insert is not the children of the current node we create it
                 current.children[i] = TrieNode()
+            #we go to the next element
             current = current.children[i]
+        #the isWord of the last element is set to True cause it is a word
         current.isWord = True
 
     def search(self, word: str) -> bool:
+        #we start at the root
         current = self.root
         for i in word:
+            #if we notice that a node has not as child a letter of the word we return False
             if not i in current.children:
                 return False
             current = current.children[i]
+        #if the last node of the word is not a Word, isWord would have been set to 0 otherwise 1
         return current.isWord
 
     def startsWith(self, prefix: str) -> bool:
