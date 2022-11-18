58bfe1524d9323cb8034d19db0685b0858cebf54
femifacia <femi.facia@epitech.eu>
2022-11-18 12:18:03 +0100

    [ADD] edit distance using levenshtein edit distance

---
+++ python/algorithms/edit_distance/main_levenshtein_distance.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        m = len(word2)
+        A = [[i + j for j in range(m + 1)] for i in range(n + 1)]
+        for i in range(n):
+            for j in range(m):
+                A[i + 1][j + 1] = min(A[i][j + 1] + 1, A[i + 1][j] + 1, A[i][j] + int(word1[i] != word2[j]))
+        return A[n][m]
