619c30aee1f5201e6d63b9b6c01c2ffea7b29bc2
femifacia <femi.facia@epitech.eu>
2023-01-08 00:39:50 +0100

    [ADD] pascal's triangle using dp

---
+++ python/algorithms/pascal's_triangle/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left = 0
+        right = n
+        while (left < right):
+            middle = (left + right) // 2
+            res = guess(middle)
+            if (res == 0):
+                return middle
+            elif res > 0:
+                left = middle + 1
+            else:
+                right = middle
+        return left
