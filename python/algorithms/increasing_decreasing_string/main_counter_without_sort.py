1c57d993f171e34f006f8d2c2bd777e19a2be4a6
femifacia <femi.facia@epitech.eu>
2023-05-19 11:24:00 +0100

    [ADD] increading decreasing string with counter and without sort

---
+++ python/algorithms/increasing_decreasing_string/main_counter_without_sort.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from collections import Counter
+
+import sys
+
+class Solution:
+    def sortString(self, s: str) -> str:
+        count = Counter(s)
+        letters_inc = "abcdefghijklmnopqrstuvwxyz"
+        letters_dec = letters_inc[::-1]
+        string = ""
+        index = 0
+        size = len(s)
+        while (index < size):
+            for i in letters_inc:
+                if count[i] > 0:
+                    string += i
+                    count[i] -= 1
+                    index += 1
+            for i in letters_dec:
+                if count[i] > 0:
+                    string += i
+                    count[i] -= 1
+                    index += 1
+        return string
+
+sol = Solution()
+string =  sys.argv[1]
+print(sol.sortString(string))
