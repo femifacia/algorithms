2ad573d5c94af2fcd4316157d6acc6a79baec4db
femifacia <femi.facia@epitech.eu>
2023-06-16 11:04:15 +0100

    [ADD] equal row and column pairs

---
+++ python/algorithms/equal_row_and_column_pairs/main_hashmap_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+
+class Solution:
+    def equalPairs(self, grid: list[list[int]]) -> int:
+        col = defaultdict(int)
+        row = defaultdict(int)
+        for i in grid:
+            string = ""
+            for j in i:
+                string += (str(j) + ',')
+            row[string] += 1
+        height = len(grid)
+        width = len(grid[0])
+        i = 0
+        while (i < width):
+            j = 0
+            string = ""
+            while(j < height):
+                string += (str(grid[j][i]) + ',')
+                j+=1
+            col[string] += 1
+            i+=1
+        ans = 0
+        for i in row:
+            if i in col:
+                ans += (col[i] * row[i])
+#        print(row,col)
+#        print(inter)
+        return ans
