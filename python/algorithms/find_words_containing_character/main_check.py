983243e8f55b0c8c2b31a3afed417498a1cbbfff
Femi FACIA <femisfacia@gmail.com>
2023-12-31 19:25:39 +0100

    [ADD] Find Words Containing Character

---
+++ python/algorithms/find_words_containing_character/main_check.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        ans = []
+        for i in range(len(words)):
+            if x in words[i]:
+                ans.append(i)
+
+        return ans
